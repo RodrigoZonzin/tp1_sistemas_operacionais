fbcd8ecf22dafb9c23f8bef0488d70c628c42f2d
RodrigoZonzin <rodrigojzonzin@gmail.com>
2023-04-22 16:56:04 -0300

    commit inicial

---
+++ ls.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]) {
+    //TIPOS DA BIBLIOTECA
+    DIR *dir; 
+    struct dirent *dir_entry; 
+
+    //CHAMADA DE SISTEMA QUE ABRE O DIRETÓRIO 
+    dir = opendir("/home/zonzin/Documentoss");
+
+    //SE FOR NULL (NAO EXISTIR, POR EXEMPLO), PRINTA UM ERRO
+    if(dir == NULL){
+        perror("opendir");
+        printf("Erro :( \n"); 
+        return 1; 
+    } 
+
+    //LÊ TODOS OS ARQUIVOS DO DIRETÓRIO ATÉ O FIM E PRINTA
+    while((dir_entry = readdir(dir)) != NULL){
+        printf("%s\n", dir_entry->d_name); 
+    }
+
+    //FECHA
+    closedir(dir); 
+
+    return 0;
+}
